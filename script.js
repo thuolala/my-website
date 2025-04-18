eeb1d6acb0ad82b59806f8fade68d9f1238258a1
thuolala <phamthushame2002@gmail.com>
2025-04-18 14:13:13 +0700

    play music

--- script.js
+++ script.js
@@ -13,3 +13,39 @@ function openWeb03() {
 function openWeb04() {
     window.open("https://thuolala.github.io/my-coffee/", "_blank");
 }
+
+let player;
+let isPlaying = true;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '0',
+        width: '0',
+        videoId: '-2yAEUp9v6M', // Only the ID (e.g., "dQw4w9WgXcQ")
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    // Nothing yet – user will click to play
+}
+
+function toggleMusic() {
+    const volOn = document.getElementById("volume-on");
+    const volOff = document.getElementById("volume-off");
+
+    if (!isPlaying) {
+        player.playVideo();
+        volOn.style.display = "none";
+        volOff.style.display = "inline-block";
+    } else {
+        player.pauseVideo();
+        volOn.style.display = "inline-block";
+        volOff.style.display = "none";
+    }
+
+    isPlaying = !isPlaying;
+}
+
